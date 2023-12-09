c10427a32c19e8bea3e5d0d2827bf62cd98c28d8
lazytadpole <temp-email-for-oauth-lazytadpole@gitlab.localhost>
2023-12-09 19:47:33 +0100

    added script to nuke everything not in vcs

--- cmake_build_files/deps/cwalk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jack/Projects/Craft/codenamesilverfish/deps/cwalk
-# Build directory: /home/jack/Projects/Craft/codenamesilverfish/cmake_build_files/deps/cwalk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
